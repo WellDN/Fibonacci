b2a83bdd3f8d13954e2c75c5357bb2f21b93638f
WellDN <Wellingtontp0@gmail.com>
2022-11-07 14:03:34 -0300

    #

---
+++ Fibonacci-numbers.ts
@@ -0,0 +1,17 @@
+//https://en.wikipedia.org/wiki/Fibonacci_number
+const nthFibonacci = (n: number):number => {
+    if (n < 0) throw "Number should be greater than 0";
+
+    if (n === 0) return 0; 
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i < n; i++) {
+        const c = a + b;
+
+        a = b;
+        b = c;
+    }
+    return b;
+}
